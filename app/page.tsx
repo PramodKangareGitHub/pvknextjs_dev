91725b87e7923b708344ae5d8e89232acbf781ad
DESKTOP-AJSUOIE\Pramod <pramodkangare1@gmail.com>
2025-01-26 16:23:50 +0530

    --Font setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Hello from "./components/hello";
-
 export default function Home() {
   console.log("Log pvk");
   return (
-    <h1 className="text-3xl font-bold ">
-      Welcome to next js
-      <Hello />
-    </h1>
+    <>
+      <h1 className="h1-bold">Welcome to next js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to next js</h1>
+    </>
   );
 }
